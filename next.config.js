21db50b2d7a1a7ee4f4d73071b4dd9d9c0fc0745
bondstreetcp <jameslyeh@gmail.com>
2026-02-21 12:40:32 -0500

    fix: ignore yahoo-finance2 Deno test deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new (require('webpack').IgnorePlugin)({
+        resourceRegExp: /^@std\/testing|^@gadicc\/fetch-mock-cache/,
+      })
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
